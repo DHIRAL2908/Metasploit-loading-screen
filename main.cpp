ddae10f8dcf300848dba0d01b16c076674d35d7d
DHIRAL <45001760+DHIRAL2908@users.noreply.github.com>
2019-06-19 11:04:57 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    for(;;){
+        string x="|";
+        Sleep(1000);system("cls");
+        cout<<x;
+        x="/";
+        Sleep(1000);system("cls");
+        cout<<x;
+        x="\\";
+        Sleep(1000);system("cls");
+        cout<<x;
+        x="|";
+        Sleep(1000);system("cls");
+        cout<<x;
+        x="/";
+        Sleep(1000);system("cls");
+        cout<<x;
+        x="\\";
+        Sleep(1000);system("cls");
+        cout<<x;
+    }
+
+}
